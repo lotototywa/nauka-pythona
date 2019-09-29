117a3a29461e961901f001f1da651235cfa10ae0
Dominik Piekarczyk <piekarczyk@gmail.com>
2019-09-29 13:24:29 +0200

    Zademonstruj wprowadzanie zmian przez innych użytkowników

--- 01_losowanie_imion.py
+++ 01_losowanie_imion.py
@@ -1,3 +1,14 @@
+"""
+    Cześć, tu Korsnislaw, lub Lotototata, czyli tata Lotototywy.
+    Pokazuję właśnie synowi, do czego służy GitHub i jak go używać.
+
+    W tej chwili mam sklonowane repozytorium i będę chciał zrobić pull requesta.
+    Pull request oznacza, że chcę żeby moje zmiany w programie były 
+    przyjęte do kodu właściela repozytorium, czyli przez Lotototywę.
+
+    No to spróbujmy teraz zapisać ten plik, zrobić commit i pull request.
+"""
+
 # random to biblioteka którą trzeba importować, 
 # niektóre biblioteki są w pakiecie pythona ale niektóre nie, a random jest
 # więc by importować random trzeba napisać import random 
