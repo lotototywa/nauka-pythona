595ec76cb5770fbeea74a44573f73ad13afdf258
Jacek Piekarczyk <jacek@MacBook-Air-Kasia.local>
2019-10-05 20:18:56 +0200

    wylosuj przedmiot komputera

--- 04_kamien_papier_nozyce.py
+++ 04_kamien_papier_nozyce.py
@@ -1,3 +1,16 @@
+"""
+Tu Lotototata. Podłączyłem się w VS Code przez Live Session do edytora Jacka.
+
+Teraz chcemy zrobić kolejny etap.
+
+Ostatnim razem zrobiliśmy listę przedmiotów i zapisywaliśmy od gracza jego wybór. 
+
+Teraz chcemy wylosować za komputer i porównać odpowiedzi.
+
+1. Losowanie (i wyświetlenie co komputer wybrał)
+2. Porównanie odpowiedzi i wyświetlenie wyniku
+"""
+
 import random
 
 przedmioty = ["kamień", "papier", "nożyce"]
@@ -14,3 +27,11 @@ wybor_gracza = input(
     )
 
 print (f"wybrałeś {wybor_gracza}")
+
+# 1. Losowanie i zapisanie wyniku losowania do zmiennej wybor_komputera
+wybor_komputera = random.choice(przedmioty)
+
+# 1b. wyświetlenie co komputer wybrał, np. Wybór komputera:" ____"
+print(f'Wybór komputera: {wybor_komputera}')
+
+# 2. Porównanie odpowiedzi
